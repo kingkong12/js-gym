060b622ccfc52d3834ea1a52c074f54f09627cc4
Mithil <vasanimithil999@gmail.com>
2025-01-02 21:08:46 +0000

    likes

--- day_4.js
+++ day_4.js
@@ -43,3 +43,58 @@ function squareDigits(num){
 
   console.log(squareDigits(9119));
 
+
+
+  // You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+
+// Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+// []                                -->  "no one likes this"
+// ["Peter"]                         -->  "Peter likes this"
+// ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+// ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+// ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+
+
+// Note: For 4 or more names, the number in "and 2 others" simply increases.
+
+
+
+// function likes(names) {
+//     const getPrefix = (plural) =>  `like${plural ?'s':''} this`;
+//     if(names.length < 2 )  return names[0] ? `${names[0]} ${getPrefix(true)}`:`no one ${getPrefix(true)}`
+    
+
+//     if(names.length === 2 ){ 
+//         names.splice(1,0,'and'); 
+//         return `${names.join(' ')} ${getPrefix()}`  
+//     }
+
+//      let prefix =  names.slice(0,2).join(', ');
+//      let middleText =  names.slice(2).length === 1  ? names.slice(2)[0] : `${names.slice(2).length} others`;
+//      return`${prefix} and ${middleText} ${getPrefix()}`
+
+//   }
+
+
+// Solution 2 
+
+function likes(names) {
+    names.length === 0 && (names = ['no one']);
+    let [a,b,c, ...others] =  names; 
+    switch(names.length){  
+        case 1: return `${a} likes this`;
+    case 2: return `${a} and ${b} like this`;
+    case 3: return `${a}, ${b} and ${c} like this`;
+    default: return `${a}, ${b} and ${others.length + 1} others like this`;
+    }
+  }
+
+
+
+
+// console.log(likes([]))
+// console.log(likes(['Peter']))
+// console.log(likes(["Max", "John"]))
+
+// console.log(likes(["Max", "John", "Mark"]))
+// console.log(likes(["Alex", "Jacob", "Mark", "Max"]))
